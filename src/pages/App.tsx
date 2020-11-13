b4ea5904674d7d94c763c543658b3322cd0b7072
Renan Guerra <rebarbosa07@hotmail.com>
2020-11-13 13:36:07 -0300

    file input

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,6 +5,14 @@ import './App.css';
 function Home() {
   const [image,setImage] = useState(String);
 
+
+  const handleChange = function loadFile(event:any) {
+    if (event.target.files.length > 0) {
+        const file = URL.createObjectURL(event.target.files[0]);
+        setImage(file);
+    }
+};
+
   return (
     <div className="App">
 
@@ -14,22 +22,22 @@ function Home() {
             </div>
 
             <div className="right">
-              <a id='link' href="">Contato</a>
-              <a id='link' href="">Donate</a>
+              <a id='link' href="/contato">Contato</a>
+              <a id='link' href="/donate">Donate</a>
             </div>
           </div>
 
       <div className="main">
         <h2 className="text1">Como funciona?</h2>
-        <h3 className="text2">Como essa página é estática você precisa upar sua imagem em algum lugar e colar o link no campo abaixo.</h3>
-        
-        <input onChange={e => setImage(e.target.value)} id='my-image' type='text' />
-        
-        <input type="button" value='Converter'/>
+        <h3 className="text2">Basta Selecionar sua imagem no campo abaixo.</h3>
+        <label className='labelInput' htmlFor="myImage"> Selecionar uma imagem.</label>
+        <input onChange={(e) => handleChange(e)} type="file" id="myImage" accept="image/*" />
 
         <div className="polaroid-hidden">
           <Polaroid img={image}/>
         </div>
+
+        <span className='labelInput'>Download</span>
 
       </div>
 
